8dd6c97156c045ae5b499171ecd534341320772c
Rick Lupton <mail@ricklupton.name>
2026-09-15 13:07:19 +0100

    Update test Python versions

--- tests/test_probs_system_directives.py
+++ tests/test_probs_system_directives.py
@@ -14,7 +14,7 @@ def test_probs_rdf_builder(app, status, warning):
     print(content)
 
     assert """<div class="system process toggle nested-1 admonition">
-<p class="sig sig-object admonition-title" id="http-example.org-system-P1"><em>Process: </em><span class="sig-name descname">P1</span><span class="sig-prename descclassname"> / Making crumble</span></p>""" in content
+<p class="admonition-title" id="http-example.org-system-P1"><em>Process: </em><span class="sig-name descname">P1</span><span class="sig-prename descclassname"> / Making crumble</span></p>""" in content
 
     assert """
 <p>Parents: <a class="reference internal" href="#http-example.org-system-ParentOfP1P2" title="http://example.org/system/ParentOfP1P2"><span>sys:ParentOfP1P2</span></a></p>""" in content
